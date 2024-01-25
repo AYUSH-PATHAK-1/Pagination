4030078843ef293ea5aedea20dba5f0f0a594cc3
AYUSH-PATHAK-1 <developerasp7@gmail.com>
2024-01-25 14:42:07 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import Data from './Data.json'
+
+const App = () => {
+  const [curruntpage,setCurrentpage]=useState(1)
+  const recordsPerpage=5;
+  const lastIndex=curruntpage*recordsPerpage;
+  const firstIndex=lastIndex-recordsPerpage;
+  const records=Data.slice(firstIndex,lastIndex);
+  const npage=Math.ceil(Data.length/recordsPerpage);
+  const numbers=[...Array(npage+1).keys()].slice(1);
+
+  function prePage(){
+    if(curruntpage !==1){
+      setCurrentpage(curruntpage-1)
+    }
+  }
+
+  function changeCpage(id){
+      setCurrentpage(id)
+  }
+
+  function nextPage(){
+      if(curruntpage !==npage){
+        setCurrentpage(curruntpage+1)
+      }
+  }
+
+  return (
+    <>
+   <div className='container mx-auto mt-12 border-black p-5 border-[2px] rounded-xl'>
+  <table className='min-w-full bg-white border border-gray-300'>
+    <thead>
+      <tr>
+        <th className='py-2 px-4 border-b text-center'>ID</th>
+        {/* <div class="line rotate-90 border-[1px] border-orange-500 md:block"></div> */}
+        <th className='py-2 px-4 border-b text-center'>Name</th>
+        <th className='py-2 px-4 border-b text-center'>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      {records.map((d, i) => (
+        <tr key={i} className={(i % 2 === 0 ? 'bg-green-100' : 'bg-white')}>
+          <td className='py-2 px-4 border-b text-center align-middle'>{d.id}</td>
+          <td className='py-2 px-4 border-b text-center align-middle'>{d.name}</td>
+          <td className='py-2 px-4 border-b text-center align-middle'>{d.email}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  <nav className="flex items-center justify-center mt-4 ">
+  <ul className="flex list-none p-2 border-black border-[1px] rounded-lg">
+    <li className="mr-3 p-2">
+      <a href="#" className="page-link" onClick={prePage}>
+        Prev
+      </a>
+    </li>
+    {numbers.map((n, i) => (
+      <li key={i} className={`mr-3 ${curruntpage === n ? 'bg-blue-500' : ''} p-2 rounded-lg`}>
+        <a href="#" className="page-link" onClick={() => changeCpage(n)}>
+          {n}
+        </a>
+      </li>
+    ))}
+    <li className='p-2'>
+      <a href="#" className="page-link" onClick={nextPage}>
+        Next
+      </a>
+    </li>
+  </ul>
+</nav>
+
+
+</div>
+
+    </>
+  )
+}
+
+export default App
